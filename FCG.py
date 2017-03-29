f56c732ceb2836cad70c1a56fbfb784760a56a84
Juan Cruz Sosa <nirvguy@gmail.com>
2017-03-29 13:34:40 -0300

    Add class to represents Free Context Grammars

---
+++ FCG.py
@@ -0,0 +1,46 @@
+class InvalidSymbol(Exception):
+    pass
+
+class InvalidName(exception):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "{} is not a valid name".format(self.name)
+
+class InvalidSymbol(InvalidName):
+    def __init__(self, name):
+        super(InvalidName, self).__init__(name)
+
+    def __str__(self):
+        return "{} is not a valid symbol".format(self.name)
+
+class InvalidToken(InvalidName):
+    def __init__(self, name):
+        super(InvalidName, self).__init__(name)
+
+    def __str__(self):
+        return "{} is not a valid token".format(self.name)
+
+class Grammar:
+    def __init__(self, tokens, start_symbol = 'START'):
+        self.S = start_symbol
+        self.tokens = tokens
+        self.rules = {}
+
+    def add_rule(self, lhs, *rhs):
+        if lhs in self.rules:
+            self.rules[lhs].add(list(rhs))
+        else:
+            self.rules[lhs] = [list(rhs)]
+
+    def __str__(self):
+        res = ""
+        for k,v in self.rules.items():
+            res += "{} ::= {}\n".format(k, " ".join(v))
+
+    def all_grammars(S = AST('START'), h=None):
+        if h is not None:
+            if S not in self.rules:
+                raise InvalidToken(S)
+
